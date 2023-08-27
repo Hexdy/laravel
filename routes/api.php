e7283f6a825911fa63aca2738bf4e986ae0f7c77
Hexdy <maximilianomarquez12@hotmail.com>
2023-08-27 04:14:07 -0300

    Commit de creacion de modelos y rutas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\API\V_1\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//Cada version debe tener una url única
+//groups se usa para poder tener una ruta específica con un grupo de endpoints, sean, por ejemplo, customers o invoices o más
+
+// API/V_1/<Endpoint>
+Route::group(['prefix' => 'V_1', 'namespace' => 'App\Http\Controllers\API\V_1'], function () {
+    Route::apiResource('customers', CustomerController::class); //Si no me equivoco ésto hará que al usar el endpoint 'customers' ejecutará el controlador de ésta, falta info
+    Route::apiResource('invoices', CustomerController::class);
+}); //La lógica es que como todos nuestros controladores de V_1 se encuentran en el mismo lugar podemos determinar namespace así
